49e47231ec9223fddf217cfcab92cd4765c2a61e
keshav <kesahvbisht006@gmail.com>
2025-07-22 10:04:35 +0530

    Initial Commit

---
+++ HealthRecord/HealthRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthRecordApp.swift
+//  HealthRecord
+//
+//  Created by Keshav Bisht on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthRecordTests/HealthRecordTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthRecordTests.swift
+//  HealthRecordTests
+//
+//  Created by Keshav Bisht on 22/07/25.
+//
+
+import Testing
+@testable import HealthRecord
+
+struct HealthRecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
