0481eba6148cf235c32ffa912669f42128794438
keshav <kesahvbisht006@gmail.com>
2025-08-04 10:54:27 +0530

    UI BUILD

---
+++ HealthRecord/NewUser.swift
@@ -0,0 +1,18 @@
+//
+//  NewUser.swift
+//  HealthRecord
+//
+//  Created by Keshav Bisht on 22/07/25.
+//
+
+import SwiftUI
+
+struct NewUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewUser()
+}
---
+++ HealthRecord/PrescriptionPage.swift
@@ -0,0 +1,18 @@
+//
+//  PrescriptionPage.swift
+//  HealthRecord
+//
+//  Created by Keshav Bisht on 23/07/25.
+//
+
+import SwiftUI
+
+struct PrescriptionPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrescriptionPage()
+}
